67cc6f1f8ae9b2d6f90a594f9074be43ff70a84d
Anuragh Baskota <anuraghbaskota@Anuraghs-MacBook-Pro.local>
2022-06-04 22:27:16 +0545

    Initilizing project

---
+++ src/main/java/com/example/hamromistiri/HamroMistiriApplication.java
@@ -0,0 +1,13 @@
+package com.example.hamromistiri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HamroMistiriApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HamroMistiriApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/hamromistiri/HamroMistiriApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hamromistiri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HamroMistiriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
