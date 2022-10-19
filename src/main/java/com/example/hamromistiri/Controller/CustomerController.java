0a6899dfb65d8b46398f5d713453ed333bf40fcf
abhirimal <abhirimal009@gmail.com>
2022-10-20 01:15:22 +0545

    create login register of mistiri, add password encryption

--- src/main/java/com/example/hamromistiri/Controller/CustomerController.java
+++ src/main/java/com/example/hamromistiri/Controller/CustomerController.java
@@ -1,13 +1,23 @@
 package com.example.hamromistiri.Controller;
 
+import com.example.hamromistiri.Dto.CustomerLoginRequest;
+import com.example.hamromistiri.Dto.CustomerSignupRequest;
+import com.example.hamromistiri.Dto.MistiriLoginRequest;
+import com.example.hamromistiri.Dto.MistiriSignupRequest;
 import com.example.hamromistiri.Model.Customer;
+import com.example.hamromistiri.Model.MistiriDetail;
 import com.example.hamromistiri.Service.CustomerServices;
+import com.example.hamromistiri.exception.AppException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpSession;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -15,10 +25,26 @@ public class CustomerController {
     @Autowired
     private CustomerServices customerServices;
 
-    @PostMapping("/registerNewCustomer")
-    public Customer registerNewCustomer(@RequestBody Customer customer) {
-        return customerServices.saveCustomer(customer);
+    @PostMapping("/registerCustomer")
+    public Customer signUpCustomer(@Valid @RequestBody CustomerSignupRequest request) throws AppException {
+        return customerServices.registerCustomer(request);
     }
+
+    @PostMapping("/loginCustomer")
+    public ResponseEntity<?> loginCustomer(@RequestBody CustomerLoginRequest request, HttpSession session) throws AppException {
+        Customer customer = customerServices.loginCustomer(request);
+        session.setAttribute("id", customer.getId());
+        session.setAttribute("email", customer.getEmail());
+        session.setAttribute("firstName", customer.getFirstName());
+        session.setAttribute("lastName", customer.getLastName());
+
+        return ResponseEntity.ok("Logged in Successfully.");
+    }
+
+//    @PostMapping("/registerNewCustomer")
+//    public Customer registerNewCustomer(@RequestBody Customer customer) {
+//        return customerServices.saveCustomer(customer);
+//    }
 
 //    @GetMapping("/findCustomer")
 //    public List<Customer> getCustomer(){
--- src/main/java/com/example/hamromistiri/Service/CustomerServices.java
+++ src/main/java/com/example/hamromistiri/Service/CustomerServices.java
@@ -1,9 +1,17 @@
 package com.example.hamromistiri.Service;
 
+import com.example.hamromistiri.Dto.CustomerLoginRequest;
+import com.example.hamromistiri.Dto.CustomerSignupRequest;
+import com.example.hamromistiri.Dto.MistiriLoginRequest;
+import com.example.hamromistiri.Dto.MistiriSignupRequest;
 import com.example.hamromistiri.Model.Customer;
+import com.example.hamromistiri.Model.MistiriDetail;
 import com.example.hamromistiri.Repository.CustomerRepository;
+import com.example.hamromistiri.exception.AppException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
 
 import java.util.List;
 
@@ -13,9 +21,53 @@ public class CustomerServices {
     @Autowired
     private CustomerRepository customerRepository;
 
-    public Customer saveCustomer(Customer customer){
+    public Customer registerCustomer(CustomerSignupRequest request) throws AppException {
+
+        Customer c = customerRepository.findByEmail(request.getEmail()).orElse(null);
+
+        if (c != null) {
+            throw new AppException("This email is already taken.", HttpStatus.BAD_REQUEST);
+        }
+
+
+        Customer customer = new Customer();
+
+        customer.setFirstName(request.getFirstName());
+        customer.setLastName(request.getLastName());
+
+//        customer.setPassword(request.getPassword());
+
+        // password encrypted and saved in database
+        String encryptedPassword = DigestUtils.md5DigestAsHex(request.getPassword().getBytes());
+        customer.setPassword(encryptedPassword);
+
+        customer.setEmail(request.getEmail());
+        customer.setPhoneNo(request.getPhoneNo());
+        customer.setRole("Customer");
+        customer.setLoction(request.getLocation());
+
+        customer = customerRepository.saveAndFlush(customer);
+
         return customerRepository.save(customer);
     }
+
+    public Customer loginCustomer(CustomerLoginRequest request) throws AppException {
+        Customer customer = customerRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new AppException("User not found for this email", HttpStatus.BAD_REQUEST));
+
+        String requestPassword = DigestUtils.md5DigestAsHex(request.getPassword().getBytes());
+        String dbPassword = customer.getPassword();
+
+        if (!requestPassword.equals(dbPassword)) {
+            throw new AppException("Invalid Password", HttpStatus.FORBIDDEN);
+        }
+        // if we throw exception then rest of the code doesn't run in a method
+        return customer;
+    }
+
+//    public Customer saveCustomer(Customer customer){
+//        return customerRepository.save(customer);
+//    }
 
     public List<Customer> getUser(){
         return customerRepository.findAll();
