56196947b52554f25e5cdf2a1872433dad87f095
abhirimal <abhirimal009@gmail.com>
2022-11-06 00:35:02 +0545

    create api of delete customer and mistiri by admin

--- src/main/java/com/example/hamromistiri/Service/AdminService.java
+++ src/main/java/com/example/hamromistiri/Service/AdminService.java
@@ -2,17 +2,33 @@ package com.example.hamromistiri.Service;
 
 import com.example.hamromistiri.Dto.AdminLoginRequest;
 import com.example.hamromistiri.Model.Admin;
-import com.example.hamromistiri.Repository.AdminRepository;
+import com.example.hamromistiri.Model.Customer;
+import com.example.hamromistiri.Model.MistiriDetail;
+import com.example.hamromistiri.Repository.*;
 import com.example.hamromistiri.exception.AppException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class AdminService {
 
     @Autowired
     private AdminRepository adminRepository;
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private MisitiriDetailRepository misitiriDetailRepository;
+
+    @Autowired
+    private ProblemRepository problemRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
 
     public Admin loginAdmin(AdminLoginRequest request){
 
@@ -33,4 +49,27 @@ public class AdminService {
 
     }
 
+    public void deleteCustomerByAdmin(int cid) {
+        misitiriDetailRepository.deleteMistiriByCustomerId(cid);
+        reviewRepository.deleteReviewByCustomerId(cid);
+        problemRepository.deleteProblemByCustomerId(cid);
+        customerRepository.deleteById(cid);
+    }
+
+
+    public void deleteMistiriByAdmin(int mid) {{
+
+        MistiriDetail mistiri = misitiriDetailRepository.findById(mid).orElse(null);
+        int customerId = mistiri.getCustomer().getId();
+        System.out.println(customerId);
+        problemRepository.deleteProblemByMistiriId(mid);
+        reviewRepository.deleteReviewByMistiriId(mid);
+        misitiriDetailRepository.deleteById(mid);
+
+        problemRepository.deleteProblemByCustomerId(customerId);
+        reviewRepository.deleteReviewByCustomerId(customerId);
+//        customerRepository.deleteById(customerId);
+
+    }
+    }
 }
