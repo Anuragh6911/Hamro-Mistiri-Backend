8219322263522c6d39d36bfda2a607ac86192fc8
Anurag Baskota <55234656+Anuragh6911@users.noreply.github.com>
2022-11-06 00:36:23 +0545

    cors origin verify

--- src/main/java/com/example/hamromistiri/Controller/AdminController.java
+++ src/main/java/com/example/hamromistiri/Controller/AdminController.java
@@ -10,12 +10,14 @@ import com.example.hamromistiri.exception.AppException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import javax.servlet.http.HttpSession;
 
 @Controller
+@CrossOrigin("*")
 public class AdminController {
 
     @Autowired
